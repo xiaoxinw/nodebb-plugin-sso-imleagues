1b8fe3587828e6ccbf179ca4ac4877c07200a0e1
xiaoxinw <xiaoxing0514@gmail.com>
2015-07-28 12:00:29 +0800

    change for nodebb v0.7.x
    make the link work on the new version,
    change some style

--- public/js/client.js
+++ public/js/client.js
@@ -3,8 +3,8 @@
  */
 $(window).on("action:ajaxify.end", function(e, t) {
     if (t.url.indexOf("login")>=0 || t.url.indexOf("register")>=0 ) {
-        var i = $("ul.alt-logins li.imleagues");
-        i.css("display", "none");
-        $('<a href="' + RELATIVE_PATH + '/auth/imleagues" class="btn btn-default"><i class="fa fa-external-link"></i> Login via IMLeagues</a>').insertAfter(i)
+        var a= $("ul.alt-logins li.imleagues a");
+        a.html('<image src="http://www.imleagues.com/images/imleagues-logo-hover.png"> Login via IMLeagues');
+	a.addClass("btn btn-primary");
     }
 });
